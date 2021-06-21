3f5a77421200ca475ed9c0a1d97098c2d087873e
EnechukwuChibuike <enechukwuchibuike@gmail.com>
2021-06-21 10:43:27 +0100

    Updates

---
+++ precache-manifest.31fbaeeb885a995180f5c8ed14aa0b56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4947887e697cdc8dcc7e2b6ff4486ef",
+    "url": "/student_dashboard/index.html"
+  },
+  {
+    "revision": "81621916c2d100ff584c",
+    "url": "/student_dashboard/static/css/main.7608a280.chunk.css"
+  },
+  {
+    "revision": "ff364a5d841f7a484dd1",
+    "url": "/student_dashboard/static/js/2.05318d6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/student_dashboard/static/js/2.05318d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81621916c2d100ff584c",
+    "url": "/student_dashboard/static/js/main.13ba5bc9.chunk.js"
+  },
+  {
+    "revision": "d71019306e9a2c1bcb4f",
+    "url": "/student_dashboard/static/js/runtime-main.018da67e.js"
+  },
+  {
+    "revision": "de8879bd8083370f05d7a5cfff15b3cf",
+    "url": "/student_dashboard/static/media/Logo.de8879bd.png"
+  },
+  {
+    "revision": "3967cb5902eebc1e464b76739bf28821",
+    "url": "/student_dashboard/static/media/user.3967cb59.png"
+  },
+  {
+    "revision": "d8716584a03872be57ecb5c17d3c7d91",
+    "url": "/student_dashboard/static/media/user.d8716584.svg"
+  }
+]);
